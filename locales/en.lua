9b5e8eeca1dd26cec185a371cdebb52d7902ca70
Demo <51279632+Demo4889@users.noreply.github.com>
2020-04-17 21:06:21 -0400

    Update en.lua
    updating to correspond with the actual item quantities needed for processing weed. also fixed a missing ~ for the color code in processing.

--- locales/en.lua
+++ locales/en.lua
@@ -5,8 +5,8 @@ Locales ['en'] = {
   ['weed_processprompt'] = 'press ~INPUT_CONTEXT~ to start ~g~Process Cannabis~s~.',
   ['weed_processingstarted'] = 'processing ~g~Cannabis~s~ into ~g~Marijuana~s~...',
   ['weed_processingfull'] = 'processing ~r~canceled~s~ due to full inventory!',
-  ['weed_processingenough'] = 'you must have ~b2x~s~ ~g~Cannabis~s~ in order to process.',
-  ['weed_processed'] = 'you\'ve processed ~b~2x~s~ ~g~Cannabis~s~ to ~b~1x~s~ ~g~Marijuana~s~',
+  ['weed_processingenough'] = 'you must have ~b~3x~s~ ~g~Cannabis~s~ in order to process.',
+  ['weed_processed'] = 'you\'ve processed ~b~3x~s~ ~g~Cannabis~s~ to ~b~1x~s~ ~g~Marijuana~s~',
   ['weed_processingtoofar'] = 'the processing has been ~r~canceled~s~ due to you abandoning the area.',
   
   -- meth
